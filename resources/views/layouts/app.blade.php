1a7b0ac1cd6163a705d3d88b8cf382d91cc1be21
SarahSibert <mail@sarahsibert.com>
2021-04-27 14:51:37 +0100

    Episode 15: Create Ideas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,32 +53,30 @@
                 <div class="bg-white md:sticky md:top-8 border-2 border-blue-500 rounded-xl mt-16">
                     <div class="text-center px-6 py-2 pt-6">
                         <h3 class="font-semibold text-base">Add an idea</h3>
-                        <p class="text-xs mt-4">Let us know what you would like and we'll take a look.</p>
+                        <p class="text-xs mt-4">
+                            @auth
+                                Let us know what you would like and we'll take a look.
+                            @else
+                                Please log in to create an idea.
+                            @endauth
+                        </p>
                     </div>
-                    <form action="#" method="POST" class="space-y-4 px-4 py-6">
-                        <div>
-                            <input type="text" class="w-full bg-gray-100 border-none rounded-xl placeholder-gray-900 px-4 py-2 text-sm" placeholder="Your Idea">
+                    @auth
+                        <livewire:create-idea />
+                    @else
+                        <div class="my-6 text-center">
+                            <a 
+                                href="{{ route('login') }}"
+                                class="inline-block justify-center w-1/2 h-11 text-xs bg-blue-500 font-semibold rounded-xl border border-blue-500 hover:bg-blue-700 transition duration-150 ease-in px-6 py-3 text-white">
+                                Log In
+                            </a>
+                            <a 
+                                href="{{ route('register') }}" 
+                                class="mt-2 inline-block justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                                    Sign Up
+                            </a>
                         </div>
-                        <div>
-                            <select name="category_add" id="category_add" class="w-full bg-gray-100 border-none rounded-xl px-4 py-2 text-sm">
-                                <option value="Category One">Category One</option>
-                                <option value="Category Two">Category Two</option>
-                                <option value="Category Three">Category Three</option>
-                            </select>
-                        </div>
-                        <div>
-                            <textarea name="idea" id="idea" cols="30" rows="4" class="w-full bg-gray-100 rounded-xl placeholder-gray-900 text-sm px-4 py-2 border-none" placeholder="Describe Your Idea"></textarea>
-                        </div>
-                        <div class="flex items-center justify-between space-x-3">
-                            <button type="button" class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
-                                <svg class="w-4 h-4 text-gray-600 transform -rotate-45" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
-                                </svg>
-                                <span class="ml-1">Attach</span>
-                            </button>
-                            <button type="submit" class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue-500 font-semibold rounded-xl border border-blue-500 hover:bg-blue-700 transition duration-150 ease-in px-6 py-3 text-white">Submit</button>
-                        </div>
-                    </form>
+                    @endauth
                 </div>
             </div>
             <!-- Main -->
