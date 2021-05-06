9220752036ba4e3c15f50eee249a4ee71cd5436d
SarahSibert <mail@sarahsibert.com>
2021-05-06 12:11:21 +0100

    Episode 31: In App Back Button
    And other tidy ups

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -1,9 +1,12 @@
 <x-app-layout>
     <!-- Filters -->
     <div>
-        <a href="/" class="flex items-cetner font-semibold hover:underline">
-            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path></svg>
-            <span class="ml-2">All Ideas</span>
+        <a href="{{ $backUrl }}" 
+            class="flex items-center font-semibold hover:underline">
+            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path>
+            </svg>
+            <span class="ml-2">All Ideas (or Back to Chosen Category with Filters)</span>
         </a>
     </div>
 
