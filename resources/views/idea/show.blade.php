b5dd966e2fbf1256e157a43eb0b0037631345a18
SarahSibert <mail@sarahsibert.com>
2021-07-09 12:04:44 +0100

    Episode 37: Modal CSS

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -15,9 +15,12 @@
          :idea="$idea"
          :votesCount="$votesCount"
      />
-    
+
+    <livewire:edit-idea />
+
     <!-- Comments Container -->
     <div class="comments-container relative space-y-6 md:ml-22 pt-4 my-8 mt-1">
+        @foreach (range(1,3) as $comment)
         <div class="comment-container relative bg-white rounded-xl flex mt-4">
             <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
                 <div class="flex-none">
@@ -38,10 +41,12 @@
                          <div 
                             x-data="{ isOpen: false}"
                             class="flex items-center space-x-2">
-                            <button 
-                                @click="isOpen = !isOpen" 
-                                class="relative bg-gray-100 hover:bg-gray-200 transition duration-150 ease-in rounded-full h-7 py-2 px-3">
-                                <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"></path></svg>
+                            <div class="relative">
+                                <button 
+                                    @click="isOpen = !isOpen" 
+                                    class="relative bg-gray-100 hover:bg-gray-200 transition duration-150 ease-in rounded-full h-7 py-2 px-3">
+                                    <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"></path></svg>
+                                </button>
                                 <ul 
                                     x-cloak
                                     x-show.transition.origin.top.left="isOpen"
@@ -51,57 +56,12 @@
                                     <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block transition duration-150 ease-in">Mark as Spam</a></li>
                                     <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block transition duration-150 ease-in">Delete Post</a></li>
                                 </ul>
-                            </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="comment-container relative bg-white rounded-xl flex mt-4">
-             <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
-                <div class="flex-none">
-                    <a href="#">
-                        <img src="{{ $idea->user->getAvatar() }}" alt="avatar" class="w-14 h-14 rounded-xl" />
-                    </a>
-                </div>
-                <div class="w-full md:mx-4">
-                    {{--
-                    <h4 class="text-xl font-semibold">
-                        <a href="#" class="hover:underline">Status Changed to "Under Consideration"</a>
-                    </h4>--}}
-                    <div class="text-gray-600 mt-3">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non soluta natus suscipit voluptatibus dolor sed velit sint voluptates quisquam, id molestias doloremque, assumenda, debitis illum iusto blanditiis incidunt ipsam vero!
-                    </div>
-                    <div class="flex items-center justify-between mt-6">
-                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
-                            <div class="font-bold text-blue-500">Jane Doe</div>
-                            <div>&bull;</div>
-                            <div>10 hours ago</div>
-                        </div>
-                        <div
-                             class="flex items-center space-x-2"
-                             x-data="{ isOpen: false }"
-                         >
-                             <button
-                                 class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3"
-                                 @click="isOpen = !isOpen"
-                             >
-                                <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"></path></svg>
-                                <ul
-                                     class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl z-10 py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0"
-                                     x-cloak
-                                     x-show.transition.origin.top.left="isOpen"
-                                     @click.away="isOpen = false"
-                                     @keydown.escape.window="isOpen = false"
-                                 >
-                                    <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block transition duration-150 ease-in">Mark as Spam</a></li>
-                                    <li><a href="#" class="hover:bg-gray-100 px-5 py-3 block transition duration-150 ease-in">Delete Post</a></li>
-                                </ul>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
 </x-app-layout>
---
+++ resources/views/livewire/edit-idea.blade.php
@@ -0,0 +1,52 @@
+<div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <div class="flex items-end justify-center min-h-screen">
+        <!-- grey background overlay -->
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+
+        <!-- MODAL -->
+        <div class="modal bg-white rounded-tl-xl rounded-tr-xl p-4 overflow-hidden transform transition-all sm:max-w-lg sm:w-full">
+            <div class="absolute top-0 right-0 pt-4 pr-4">
+                <button class="text-gray-400 hover:text-gray-500">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                </button>
+            </div>
+            <div class="bg-white px-4 pt-5 sm:p-6 sm:pb-4">
+                <h3 class="text-center text-lg font-medium text-gray-900">Edit Idea</h3>
+                <p class="text-xs text-center text-gray-500 mt-4 px-6 ">You have one hour to edit your hour after you have created it.</p>
+                <form wire:submit.prevent="createIdea" action="#" method="POST" class="space-y-4 px-4 py-6">
+                    <div>
+                        <input wire:model.defer="title" type="text" class="w-full bg-gray-100 border-none rounded-xl placeholder-gray-900 px-4 py-2 text-sm" placeholder="Your Idea">
+                        @error('title')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div>
+                        <select wire:model.defer="category" name="category_add" id="category_add" class="w-full bg-gray-100 border-none rounded-xl px-4 py-2 text-sm">
+                                <option value="1">Category 1</option>
+                        </select>
+                        @error('category')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div>
+                        <textarea wire:model.defer="description" name="idea" id="idea" cols="30" rows="4" class="w-full bg-gray-100 rounded-xl placeholder-gray-900 text-sm px-4 py-2 border-none" placeholder="Describe Your Idea"></textarea>
+                        @error('description')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <!-- Buttons -->
+                    <div class="flex items-center justify-between space-x-3">
+                        <button type="button" class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                            <svg class="w-4 h-4 text-gray-600 transform -rotate-45" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
+                            </svg>
+                            <span class="ml-1">Attach</span>
+                        </button>
+                        <button type="submit" class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue-500 font-semibold rounded-xl border border-blue-500 hover:bg-blue-700 transition duration-150 ease-in px-6 py-3 text-white">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
